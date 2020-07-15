bc31b0d6c17cc24b8cafae82a2bbc507f4d72d73
le_grand_mannitout <ouaksel.madil@gmail.com>
2020-07-15 17:30:55 +0200

    OBP, dict but still # bug

--- baseball_algo.py
+++ baseball_algo.py
@@ -4,6 +4,8 @@ import bs4
 import pickle
 import sys
 
+#del save function and replace AB/G with OBP, in a dict
+
 def team():
 
     teams = []
@@ -12,9 +14,6 @@ def team():
 
     soup = bs4.BeautifulSoup(url_season.text, 'html.parser')
     teams_soup = soup.findAll("th", {"class":"left"})
-    OBP_soup = soup.findAll("td", {"data-stat":"onbase_perc"})[:30]
-
-    OBP_list = soup_to_data(OBP_soup)
 
     for i in range(32):
 
@@ -26,7 +25,7 @@ def team():
         else :
             teams[-1] = teams[-1]["href"]
 
-    return teams, OBP_list
+    return teams
 
 
 def soup_to_data(soup_name):
@@ -39,53 +38,38 @@ def soup_to_data(soup_name):
 
     return clean_soup
 
-def save(stats):
+def player_stat(teams):
 
-    #sys.setrecursionlimit()
-
-    with open('C:/Users/Madil/Documents/Programmation/Python/Visual_studio/baseball_algo/save_pickle.txt', 'wb') as f:
-        pickle.dump(stats[0][:2], f)
-
-def player_stat(teams, OBP_list):
-
-    stats = []
+    stats = {}
     
     for i in range(len(teams)):
 
-        at_bat_per_match = []
-        AB_list = []
-        G_list = []
         player_list = []
+        OBP_list = []
+        list_tuple = []
 
         url_team = requests.get('https://www.baseball-reference.com' + teams[i])
         
         soup = bs4.BeautifulSoup(url_team.text, 'html.parser')
         player_soup = soup.findAll("td", {"data-stat":"player"})[:45]
-        AB_soup = soup.findAll("td", {"data-stat":"AB"})[:45]
-        G_soup = soup.findAll("td", {"data-stat":"G"})[:45]
+        OBP_soup = soup.findAll("td", {"data-stat":"onbase_perc"})[:45]
 
-        AB_list = soup_to_data(AB_soup)
-        G_list = soup_to_data(G_soup)
+        OBP_list = soup_to_data(OBP_soup)
         player_list = soup_to_data(player_soup)
 
+        print(player_list)
+
         if i == 25 :
-            del G_list[44:]
+            del OBP_list[44:]
             del player_list[44:]
 
         for k in range(len(player_list)):
 
-            if int(G_list[k]) != 0 :
-            
-                at_bat_per_match.append(int(AB_list[k]) / int(G_list[k]))
-            
-            else :
-                at_bat_per_match.append(0)
+            list_tuple.append((player_list[k], OBP_list[k]))
 
-        stats.append([teams[i], OBP_list[i], player_list, at_bat_per_match])
+        stats[teams[i]] = list_tuple
 
     return stats
 
-teams, OBP_list = team()
-stats = player_stat(teams, OBP_list)
-
-save(stats)
+teams = team()
+stats = player_stat(teams)
