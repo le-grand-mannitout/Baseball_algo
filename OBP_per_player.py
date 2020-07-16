668cd0292b1254b4d694655937e69f0d21f9bcdd
le_grand_mannitout <ouaksel.madil@gmail.com>
2020-07-16 12:24:37 +0200

    OBP per player (all players)

--- OBP_per_player.py
+++ OBP_per_player.py
@@ -14,16 +14,18 @@ def team():
 
     soup = bs4.BeautifulSoup(url_season.text, 'html.parser')
     teams_soup = soup.findAll("th", {"class":"left"})
+    nb_batter_per_team_soup = soup.findAll("td", {"data-stat":"batters_used"})
+
+    nb_batter_per_team = soup_to_data(nb_batter_per_team_soup[:30])
 
     for i in range(32):
 
-        teams.append(teams_soup[i].find("a"))
-
-        if teams[-1] == None:
-            del teams[-1]
+        if teams_soup[i].find("a") == None:
+            pass
 
         else :
-            teams[-1] = teams[-1]["href"]
+            teams.append([teams_soup[i].find("a"), nb_batter_per_team[i]])
+            teams[i][0] = teams[i][0]["href"]
 
     return teams
 
@@ -32,7 +34,7 @@ def soup_to_data(soup_name):
 
     clean_soup = []
 
-    for i in range (len(soup_name)):
+    for i in range(len(soup_name)):
 
         try :
             clean_soup.append(soup_name[i].string)
@@ -52,16 +54,13 @@ def player_stat(teams):
         player_soup = []
         player_list = []
         OBP_list = []
+        nb_batter = int(teams[i][1])
 
-        url_team = requests.get('https://www.baseball-reference.com' + teams[i])
+        url_team = requests.get('https://www.baseball-reference.com' + teams[i][0])
         
         soup = bs4.BeautifulSoup(url_team.text, 'html.parser')
-        player_soup_mess = soup.findAll("td", {"data-stat":"player"})[:45]
-        OBP_soup = soup.findAll("td", {"data-stat":"onbase_perc"})[:45]
-
-        if i == 25 :
-            del OBP_list[44:]
-            del player_list[44:]
+        player_soup_mess = soup.findAll("td", {"data-stat":"player"})[:nb_batter]
+        OBP_soup = soup.findAll("td", {"data-stat":"onbase_perc"})[:nb_batter]
 
         for j in range(len(player_soup_mess)):
             player_soup.append(player_soup_mess[j].find("a"))
